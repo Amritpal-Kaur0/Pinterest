74b725a07ab338fde45a01c216d167111d0240f7
Amrit1-kaur <amrit.gill3005@gmail.com>
2023-11-16 17:46:53 +0530

    post model added

---
+++ routes/posts.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/pinterest')
+
+const postSchema = new mongoose.Schema({
+ user:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+ },
+  postText: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: {
+    type: Array,
+    default: [],
+  },
+});
+
+module.exports = mongoose.model('Post', postSchema);
